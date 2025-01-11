2eb4e03e766bdc03b7bb209423e3f821c8dee833
Atemo-C <c.roux1993@gmail.com>
2025-01-11 17:37:22 +0100

    Minor updates.

--- fish/config.fish
+++ fish/config.fish
@@ -1,10 +1,10 @@
-# Commands to run in interactive sessions can go here
+# Commands to run in interactive sessions can go here.
 if status is-interactive
 end
 
-# Set a custom "greetings" message
+# Set a custom "greetings" message.
 set fish_greeting
 
-# Functin to make the famous !! work as our lord and saviour the Linux Fox intended
+# Functin to make the famous !! work as our lord and saviour the Linux Fox intended.
 function last_history_item; echo $history[1]; end
 abbr -a !! --position anywhere --function last_history_item
